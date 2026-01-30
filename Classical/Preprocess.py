1d425cabdcd2e95e4d89bd72676efd5ddc841c4e
Kaitlyn Sanchez <kaitlyn_sanchez@mines.edu>
2026-01-29 18:19:50 -0700

    Created correct target column using filtered data set and created a separate array to store all events that had a known time to next slip for training purposes.
    Note: the target column is not the correct date-time object yet, and the notebooks have not been adapted for additional input to the preprocessing function, and the official training/testing split has not been done yet. Therefore, the preprocessing as is will break all the notebooks.

--- Classical/Preprocess.py
+++ Classical/Preprocess.py
@@ -4,19 +4,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import mean_absolute_error
 
-def preprocess_data(data):
-    # Checking the data has loaded as expected. Un-comment to check.
-    #print(data.head())
-    #print(data.shape)
+#Where A1 is filtered time to next slip matrix and A2 is the full data set with features 
 
+def preprocess_data(filtered_time, data_orig):
+    filter_mask = filtered_time.iloc[:,1] != -1 #looking at column 2 of the time filtered data and keeping only the rows that are not -1
+    TTNS = filtered_time.loc[filter_mask]
+    data = data_orig.loc[filter_mask]
+#Putting all the events that have a known time until next slip into there own array so that we can train only on known slips 
+    collect_known_slips = (filtered_time.iloc[1:,1] != -1).to_numpy()
+    known_next_slips = data_orig[:-1][collect_known_slips]
+    amount_of_known = known_next_slips.shape
+
+#This is breaking the notebooks and needs to be fixed:
+#Should the time to next event and time since both be converted to hours?
+#Also will need to update the preprocessing function in the other notebooks 
+    TTNS["time_to_next_ev_hr"] = pd.to_datetime(TTNS["time_to_next_ev_hr"]).dt.total_seconds() # converting to date/time object
+    #TTNS["time_to_next_ev_hr"] = TTNS["time_to_next_ev_hr"].dt.total_seconds() #Putting the time interval in seconds
+    '''
     # Constructing the target column
     data = data.dropna(subset=["start_time"]) #removing any place holder rows (such as rows of all zeros) before sorting by dropping rows with no time stamp
     data["start_time"] = pd.to_datetime(data["start_time"]) # converting to date/time object
-    data = data.sort_values("start_time").reset_index(drop=True) #ordering chronologically and giving new index (for target column and test/training split)
-    data["TTNS"] = data["time_since"].shift(-1)  #Shifting the time-since column up by one to make time-to-next-slip 
     #data["start_time"] = data["TTNS"].dt.total_seconds() #Putting the time interval in seconds
     data = data.iloc[:-1] #Not including the last row since there isn't a "next event" for it
-
+    '''
     # Sanity checks
     #nan_rows = data[data["TTNS"].isna()]
     #print(nan_rows)
@@ -31,10 +41,9 @@ def preprocess_data(data):
     feature_cols = ["tide_deriv", "form_fac", "slip_size", "high_t_evt", "tide_height", "time_since"]
 
     X = data[feature_cols] # feature columns/variables
-    y = data["TTNS"] # target column
+    y = TTNS # target column
 
-    #y = np.log10(data["TTNS"]) #use for improved regression models since we're spanning multiple orders of magnitude
-
+#Still need to find a different way to split it 
     n = len(data)
     train_end = int(0.7*n) # 70 % of data for training
     val_end = int(0.85*n) #taking another 15% for validation
@@ -48,5 +57,5 @@ def preprocess_data(data):
     X_test = X.iloc[val_end:] #the final 15% is for testing
     y_test = y.iloc[val_end:]
     
-    return X_train, X_val, X_test, y_train, y_val, y_test, feature_cols
+    return X_train, X_val, X_test, y_train, y_val, y_test, feature_cols, amount_of_known
     
