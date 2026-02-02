202c53c9d53a00030b03e08a9578cdde857827bf
Kaitlyn Sanchez <kaitlyn_sanchez@mines.edu>
2026-02-02 14:10:33 -0700

    Filtering updated to account for unknown time since last events

--- Classical/Preprocess.py
+++ Classical/Preprocess.py
@@ -11,12 +11,13 @@ from sklearn.preprocessing import StandardScaler
 def preprocess_data(filtered_time, data_orig):
     filter_mask = filtered_time["time_to_next_ev_hr"] != -1 #looking at column 2 of the time filtered data and keeping only the rows that are not -1
     print(filter_mask)
-    TTNS = filtered_time[filter_mask.shift(1) & filter_mask][:-1]
+    TTNS = filtered_time[filter_mask.shift(1) & filter_mask & filter_mask.shift(-1)][1:-1]
     print(TTNS.shape)
     data = data_orig.loc[filter_mask]
 
 #Putting all the events that have a known time until next slip into there own array so that we can train only on known slips 
-    known_next_slips = data_orig[filter_mask.shift(1) & filter_mask][:-1]
+    #known_next_slips = data_orig[filter_mask.shift(1) & filter_mask][:-1] 
+    known_next_slips = data_orig[filter_mask.shift(1) &  filter_mask & filter_mask.shift(-1)][1:-1]
     amount_of_known = known_next_slips.shape
     print(known_next_slips.shape)
     print(data_orig.shape)
